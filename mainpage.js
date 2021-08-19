70ee7f52264e20b2ebc789b7e2d89416a8d355a6
bl0ssom <86112607+bl0ssom@users.noreply.github.com>
2021-08-18 19:44:06 -0500

    Add files via upload

---
+++ mainpage.js
@@ -0,0 +1,20 @@
+function planes() {
+    window.location.href = "file:///Users/zoranpan/code/Website/planes.html"
+  }
+
+  function vehicles() {
+      window.location.href = "file:///Users/zoranpan/code/Website/land%20vehicles.html"
+  }
+
+function robots() {
+    window.location.href = "file:///Users/zoranpan/code/Website/robots.html"
+}
+
+function legos() {
+    window.location.href = "file:///Users/zoranpan/code/Website/legos.html"
+}
+
+
+function dragon() {
+   window.location.href = "https://www.amazon.com/LEGO-Ninjago-Dragon-Figurine-Building/dp/B071LB2K8D/ref=sr_1_10?dchild=1&keywords=lego+ninjago+dragon&qid=1629120914&s=toys-and-games&sr=1-10"
+}
