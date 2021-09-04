43d5baa914f87faaadcd64d2f6b158cb842f1c15
bl0ssom <86112607+bl0ssom@users.noreply.github.com>
2021-09-04 15:51:08 -0500

    Add files via upload

---
+++ legos.js
@@ -0,0 +1,30 @@
+
+
+ function bastion() {
+    window.location.href = "https://www.amazon.com/LEGO-6250958-Overwatch-Building-Multicolor/dp/B07G5Y1WRM/ref=sr_1_1?dchild=1&gclid=Cj0KCQjwpf2IBhDkARIsAGVo0D2hQWUPnlnMfo7I0lDtov9j3TMNQfttiQvfjLUGHm0zBMSeAZzQVbQaAgWgEALw_wcB&hvadid=323100320846&hvdev=c&hvlocphy=9022887&hvnetw=g&hvqmt=e&hvrand=4088066979271856910&hvtargid=kwd-634104736994&hydadcr=2994_9919017&keywords=bastion+lego+set&qid=1629464067&sr=8-1"
+ }
+
+
+ function flybot() {
+     window.location.href = "https://www.amazon.com/LEGO-6099961-Creator-Future-Flyers/dp/B00NHQGGXY/ref=sr_1_10?dchild=1&keywords=lego+3+in+1&qid=1629034086&refinements=p_n_feature_ten_browse-bin%3A3154165011%2Cp_36%3A1253563011&rnid=386491011&s=toys-and-games&sr=1-10"
+ }
+
+ function atst() {
+     window.location.href = "https://www.amazon.com/LEGO-Star-AT-ST-Walker-75153/dp/B01CVGVB4O/ref=sr_1_2?dchild=1&keywords=lego+star+wars+at+st&qid=1629120701&refinements=p_89%3ALEGO&rnid=2528832011&s=toys-and-games&sr=1-2"
+ }
+
+function legobox() {
+    window.location.href = "https://www.amazon.com/LEGO-Classic-Medium-Creative-Brick/dp/B00NHQFA1I/ref=sr_1_1?dchild=1&keywords=lego+box+set&qid=1629120741&s=toys-and-games&sr=1-1"
+}
+
+function republic() {
+    window.location.href = "https://www.amazon.com/LEGO-Republic-Fighter-75182-Building/dp/B06XRGBBXP/ref=sr_1_17?dchild=1&keywords=lego+star+wars+clone+wars+sets&qid=1629120805&s=toys-and-games&sr=1-17"
+}
+
+function dragon() {
+    window.location.href = "https://www.amazon.com/LEGO-Ninjago-Dragon-Figurine-Building/dp/B071LB2K8D/ref=sr_1_10?dchild=1&keywords=lego+ninjago+dragon&qid=1629120914&s=toys-and-games&sr=1-10"
+}
+
+function battlepack() {
+    window.location.href = " https://www.amazon.com/LEGO-Star-Wars-Specialists-Building/dp/B075QVKRSK/ref=pd_bxgy_img_2/142-8851962-8613215?pd_rd_w=FOBNY&pf_rd_p=c64372fa-c41c-422e-990d-9e034f73989b&pf_rd_r=YPQJCHHXYMKBNQ7B2NZG&pd_rd_r=f747fd19-baa8-4c24-aa6a-e4c9fadddc2e&pd_rd_wg=Pnuxe&pd_rd_i=B075QVKRSK&psc=1"
+}
--- mainpage.js
+++ mainpage.js
@@ -1,20 +1,16 @@
+  
 function planes() {
-    window.location.href = "file:///Users/zoranpan/code/Website/planes.html"
+    window.location.href = "file:///Users/zoran/code/Website/planes.html"
   }
 
   function vehicles() {
-      window.location.href = "file:///Users/zoranpan/code/Website/land%20vehicles.html"
+      window.location.href = "file:///Users/zoran/code/Website/land%20vehicles.html"
   }
 
 function robots() {
-    window.location.href = "file:///Users/zoranpan/code/Website/robots.html"
+    window.location.href = "file:///Users/zoran/code/Website/robots.html"
 }
 
 function legos() {
-    window.location.href = "file:///Users/zoranpan/code/Website/legos.html"
-}
-
-
-function dragon() {
-   window.location.href = "https://www.amazon.com/LEGO-Ninjago-Dragon-Figurine-Building/dp/B071LB2K8D/ref=sr_1_10?dchild=1&keywords=lego+ninjago+dragon&qid=1629120914&s=toys-and-games&sr=1-10"
+    window.location.href = "file:///Users/zoran/code/Website/legos.html"
 }
